336a21830fba4e81b5c4659c30b77fd0d1950e4b
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-05-03 20:02:09 -0400

    Update ubuntu.sh

--- install-scripts/ubuntu.sh
+++ install-scripts/ubuntu.sh
@@ -1,4 +1,4 @@
 # Need to run all as sudo 
 apt-get update
 apt-get upgrade
-apt-get -y install default-jdk git maven nodejs gradle ant curl 
+apt-get -y install default-jdk git maven nodejs gradle ant curl chromium-browser
