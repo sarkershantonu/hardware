0b1cb3d6b30c7c2fc85953d61c7a77c74012dd93
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-06-02 04:04:39 -0400

    Update ubuntu.sh

--- install-scripts/ubuntu.sh
+++ install-scripts/ubuntu.sh
@@ -1,11 +1,11 @@
 # Need to run all as sudo 
 apt-get -y update
 #for chrome stable
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key -y add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 # for oracle jdk8
-add-apt-repository ppa:webupd8team/java
+add-apt-repository -y ppa:webupd8team/java
 
 apt-get -y update && apt-get -y upgrade
 apt-get -y install default-jdk git maven nodejs gradle ant curl chromium-browser google-chrome-stable smbclient 
