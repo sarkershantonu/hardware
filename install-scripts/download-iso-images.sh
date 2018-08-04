1c792eaabfea508afc218c1cf7e224bfde722a4a
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-08-03 23:57:47 -0400

    Update download-iso-images.sh

--- install-scripts/download-iso-images.sh
+++ install-scripts/download-iso-images.sh
@@ -3,3 +3,29 @@
 # -A(pattern)
 # -r is recursive
 wget -r -l3 -A.iso http://cdimage.ubuntu.com/ubuntu-mate/releases/
+wget -r -l1 -A.xz http://download.proxmox.com/images/system/
+wget -r -l1 -A.gz http://download.proxmox.com/images/system/
+
+wget -r -l1 -A.iso http://cdimage.kali.org/kali-2018.2/
+
+wget -r -l2 -A.iso http://mirror.sjc02.svwh.net/ubuntu-releases/
+
+wget -r -l2 -A.iso http://cdimage.ubuntu.com/lubuntu/releases/18.04/release/
+wget -r -l2 -A.iso http://cdimage.ubuntu.com/lubuntu/releases/
+wget -r -l3 -A.iso http://cdimage.ubuntu.com/ubuntu-mate/releases/
+wget -r -l2 -A.iso http://mirrors.evowise.com/linuxmint/stable/
+
+#Experiment 
+wget -r -l4 -A.iso http://cdimage.ubuntu.com/
+
+# Debian
+wget -r -l3 -A.iso https://cdimage.debian.org/cdimage/release/
+
+wget -r -l4 -A.iso https://cdimage.debian.org/cdimage/archive/
+
+cent os
+http://ftp.usf.edu/pub/centos/6/isos/x86_64/
+http://ftp.usf.edu/pub/centos/7/isos/x86_64/
+
+wget -r -l1 -A.iso http://cdimage.ubuntu.com/ubuntu-mate/releases/16.04.4/release/
+wget -r -l3 -A.iso http://cdimage.ubuntu.com/ubuntu-mate/releases/
