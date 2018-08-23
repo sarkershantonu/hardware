f412f1c80994ed32e3e478325323659ce0625b59
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-08-22 21:43:06 -0400

    Update install-u16.sh

--- install-scripts/install-u16.sh
+++ install-scripts/install-u16.sh
@@ -1,3 +1,3 @@
 # Ubuntu 16.04 LTS auto configurer 
 apt-get -y update && apt-get -y upgrade
-apt-get -y install default-jdk git maven nodejs gradle ant curl smbclient software-properties-common p7zip-full
+apt-get -y install default-jdk git maven nodejs gradle ant curl smbclient software-properties-common p7zip-full cifs-utils
