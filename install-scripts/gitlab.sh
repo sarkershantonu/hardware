851efe431bd2e6535fe37d63829d6babcafe709a
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-08-26 00:17:30 -0400

    Create gitlab.sh

---
+++ install-scripts/gitlab.sh
@@ -0,0 +1,11 @@
+#Git Lab 
+apt-get update
+apt-get install -y curl openssh-server ca-certificates postfix
+
+#Configure mail server before installtion, i used to skip. 
+#gitlab-ctl reconfigure
+
+curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
+#Use your local server url in ecternal url, this is my example
+EXTERNAL_URL="http://gitlab.shantonu.home" apt-get install gitlab-ce
+
