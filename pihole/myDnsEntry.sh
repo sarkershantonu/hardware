27ef2cf7f86768aab565d3b7d180bcffe0c2b8fb
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-09-11 18:33:19 -0400

    myLocalDNS adding after installation

---
+++ pihole/myDnsEntry.sh
@@ -0,0 +1,5 @@
+pihole -a -r pihole.shantonu.home 192.168.1.9
+pihole -a -r nexus.shantonu.home 192.168.1.10
+pihole -a -r jenkins.shantonu.home 192.168.1.11
+pihole -a -r gitlab.shantonu.home 192.168.1.12
+pihole -a -r mysql.shantonu.home 192.168.1.15
