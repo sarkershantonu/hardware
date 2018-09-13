12124b48cc6425373fc3445906af992e00cbc031
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-09-13 01:47:58 -0400

    Update myDnsEntry.sh

--- pihole/myDnsEntry.sh
+++ pihole/myDnsEntry.sh
@@ -3,3 +3,5 @@ pihole -a -r nexus.shantonu.home 192.168.1.10
 pihole -a -r jenkins.shantonu.home 192.168.1.11
 pihole -a -r gitlab.shantonu.home 192.168.1.12
 pihole -a -r mysql.shantonu.home 192.168.1.15
+pihole -a -r plex.shantonu.home 192.168.1.16
+pihole -a -r opencart.shantonu.home 192.168.1.17
