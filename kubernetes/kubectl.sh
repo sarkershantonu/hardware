a1d79ffc8153364b511086bab49b6babdb16e362
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-09-24 11:37:09 -0400

    Update kubectl.sh

--- kubernetes/kubectl.sh
+++ kubernetes/kubectl.sh
@@ -5,4 +5,4 @@ touch /etc/apt/sources.list.d/kubernetes.list
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
-apt-get bash-completion -y kubectl
+apt-get install -y bash-completion
