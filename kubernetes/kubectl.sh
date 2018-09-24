5be4672d3fae43bb627bf96d81982130ca761fb0
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-09-24 11:30:45 -0400

    kubectl local host

---
+++ kubernetes/kubectl.sh
@@ -0,0 +1,7 @@
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
