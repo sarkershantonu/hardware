73ef0f441d912d81d426193c4ed2c28768f82c3b
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-09-29 02:08:50 -0400

    Create add-repo.sh

---
+++ OpenMediaVault/add-repo.sh
@@ -0,0 +1,8 @@
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb http://packages.openmediavault.org/public erasmus main
+deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus main
+deb http://packages.openmediavault.org/public erasmus-proposed main
+deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus-proposed main
+deb http://packages.openmediavault.org/public erasmus partner
+deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus partner
+EOF
