06dc8f86de39fd6f21f2bdab9c4ae017cb31fdf3
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-09-29 02:09:45 -0400

    Create add-repo.sh

---
+++ OpenMediaVault/deb9/add-repo.sh
@@ -0,0 +1,8 @@
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb http://packages.openmediavault.org/public arrakis main
+deb http://downloads.sourceforge.net/project/openmediavault/packages arrakis main
+deb http://packages.openmediavault.org/public arrakis-proposed main
+deb http://downloads.sourceforge.net/project/openmediavault/packages arrakis-proposed main
+deb http://packages.openmediavault.org/public arrakis partner
+deb http://downloads.sourceforge.net/project/openmediavault/packages arrakis partner
+EOF
