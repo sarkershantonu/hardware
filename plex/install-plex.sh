d71113e1a00296aacc6f21e8bbf2f5797aac8943
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-10-03 23:06:01 -0400

    Update install-plex.sh

--- plex/install-plex.sh
+++ plex/install-plex.sh
@@ -1,1 +1,6 @@
-# Installation 
+# Installation for ubuntu/debian
+apt-get update && apt-get -y upgrade 
+wget https://downloads.plex.tv/plex-media-server/1.13.8.5395-10d48da0d/plexmediaserver_1.13.8.5395-10d48da0d_amd64.deb
+dpkg -i plexmediaserver_1.7.5.4035-313f93718_amd64.deb
+systemctl start plexmediaserver
+systemctl enable plexmediaserver
