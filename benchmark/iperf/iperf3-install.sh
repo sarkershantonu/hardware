e110d57812c84e2d8b56b0a94c26ea5c002a0fff
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-10-06 06:04:47 -0400

    Create iperf3-install.sh

---
+++ benchmark/iperf/iperf3-install.sh
@@ -0,0 +1,5 @@
+apt-get -y purge iperf3 libiperf0
+wget https://iperf.fr/download/ubuntu/libiperf0_3.1.3-1_amd64.deb
+wget https://iperf.fr/download/ubuntu/iperf3_3.1.3-1_amd64.deb
+dpkg -i -y libiperf0_3.1.3-1_amd64.deb iperf3_3.1.3-1_amd64.deb
+rm libiperf0_3.1.3-1_amd64.deb iperf3_3.1.3-1_amd64.deb
