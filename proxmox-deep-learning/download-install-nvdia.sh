c56333f395f0774013324da4e5b1f585eca74bf8
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-10-06 04:56:57 -0400

    Create download-install-nvdia.sh

---
+++ proxmox-deep-learning/download-install-nvdia.sh
@@ -0,0 +1,4 @@
+apt-get -y purge nvidia* 
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.87/NVIDIA-Linux-x86_64-390.87.run
+chmod +x NVIDIA-Linux-x86_64-390.87.run
+./NVIDIA-Linux-x86_64-390.87.run
