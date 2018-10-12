d4b7ed13b0b479b8da65452c74666276f2a19a72
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-10-12 00:26:25 -0400

    Update java8.sh

--- install-scripts/java8.sh
+++ install-scripts/java8.sh
@@ -1,4 +1,4 @@
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
-apt-get install oracle-java8-installer oracle-java8-set-default
+apt-get install -y oracle-java8-installer oracle-java8-set-default
 
