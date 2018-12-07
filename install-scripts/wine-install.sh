0de8da25213070785401345b92b6cc7bae880616
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2018-12-06 21:24:51 -0500

    Update wine-install.sh

--- install-scripts/wine-install.sh
+++ install-scripts/wine-install.sh
@@ -1,3 +1,8 @@
 add-apt-repository -y ppa:ubuntu-wine/ppa
 apt-get update
 apt-get install -y wine1.8 winetricks
+dpkg --add-architecture i386
+
+add-apt-repository -y ppa:wine/wine-builds
+apt-get update
+sudo apt-get install --install-recommends winehq-devel
