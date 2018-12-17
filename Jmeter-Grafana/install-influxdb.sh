a8f28b957f36d1ed79f5a8d0ab79cfc9f5bdf923
Shantonu Sarker <shantonu_oxford@yahoo.com>
2018-12-17 01:44:07 -0500

    Update install-influxdb.sh

--- Jmeter-Grafana/install-influxdb.sh
+++ Jmeter-Grafana/install-influxdb.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+# Single file Installer to install & run as service in ubuntu
+apt-get update && apt-get install -y gnupg curl
 
-wget https://dl.influxdata.com/influxdb/releases/influxdb_1.7.2_amd64.deb
-dpkg -i influxdb_1.7.2_amd64.deb
+curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
+
+source /etc/lsb-release
+
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+apt-get update && apt-get install -y influxdb
+
+systemctl unmask influxdb.service
+
+systemctl enable influxdb.service
+
+systemctl start influxdb.service
+
+service influxdb status
